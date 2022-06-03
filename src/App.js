3c4cede70702ffefd6e8c9ba6f9a678bd9195439
lemonmeringue93 <100950641+lemonmeringue93@users.noreply.github.com>
2022-06-03 19:19:45 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import './App.css';
 export default function App () {
   return (
     <div className="App">
-      <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" />
-     <h1>Weather App</h1>
-     <p>is this thing on?</p>
+      <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="" />
+     <h1>18 </h1>
+     <footer>
+       This project was coded by <a href="https://www.instagram.com/bellebellebelle_/?hl=en">Belle Zamora</a> and is <a href="https://github.com/lemonmeringue93/react-weather-app">open-sourced on Github.</a>
+     </footer>
     </div>
   );
 }
