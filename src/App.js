f06689e7cda905ae19a6791a99958f11ae402f53
lemonmeringue93 <100950641+lemonmeringue93@users.noreply.github.com>
2022-06-03 19:16:06 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 export default function App () {
   return (
     <div className="App">
+      <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" />
      <h1>Weather App</h1>
+     <p>is this thing on?</p>
     </div>
   );
 }
